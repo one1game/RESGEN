3d14e182c46fdae9518d04ec741a66b47685d118
PhpDead <igoralx9119@gmail.com>
2025-11-08 19:14:04 +0500

    1

--- js/ui.js
+++ js/ui.js
@@ -213,21 +213,8 @@ function render() {
     applyCollapsedState();
     updateVoiceControls();
 
-    // Скрываем/показываем апгрейды по квестам
-    const miningCard = document.querySelector('.upgrade-card:nth-child(1)');
-    const defenseCard1 = document.querySelector('.upgrade-card:nth-child(2)');
-    const defenseCard2 = document.querySelector('.upgrade-card:nth-child(3)');
     
-    if (miningCard) miningCard.style.display = chipsUnlocked ? 'block' : 'none';
-    if (defenseCard1) defenseCard1.style.display = plasmaUnlocked ? 'block' : 'none';
-    if (defenseCard2) defenseCard2.style.display = plasmaUnlocked ? 'block' : 'none';
-
-    renderQuests();
-    renderTrade();
-    applyCollapsedState();
-    updateVoiceControls();
 }
-
 
 function updateVoiceControls() {
     if (toggleVoiceBtn) {
